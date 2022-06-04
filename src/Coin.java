eca3d243ba48ce4f7aa119d3c1f299969a15c875
M1ttwsuf <ttwsuf@naver.com>
2022-06-04 23:11:11 +0900

    init

---
+++ src/Coin.java
@@ -0,0 +1,11 @@
+/*stackLIFO 자료구조*/
+public class Coin {
+    private int value;
+
+    public Coin (int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+}
